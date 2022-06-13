93239fe1d99fd2063dfb2ee4aecfdb926fcc55f4
brie <briememo95@gmail.com>
2022-06-13 13:02:37 +0300

    add styling

--- peerrev/settings.py
+++ peerrev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(k5w#wrw$q6j%&6=29=_lj5jscam2hb)8ooy1=lu*fk1c%exi6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
