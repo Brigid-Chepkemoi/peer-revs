467150bc974a32531f438edc0902183b7b9c46d0
brie <briememo95@gmail.com>
2022-06-14 17:10:50 +0300

    changes cloudinary configs

--- peerrev/settings.py
+++ peerrev/settings.py
@@ -167,5 +167,6 @@ LOGOUT_REDIRECT_URL = 'django_registration_register'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configure Django App for Heroku.
+
 django_heroku.settings(locals())
+
